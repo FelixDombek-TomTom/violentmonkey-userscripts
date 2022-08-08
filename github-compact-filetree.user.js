108a5f69a95eb357930f7196fa1790821442fdd7
Felix Dombek <Felix.Dombek@tomtom.com>
2022-08-09 00:45:28 +0200

    extend scope

--- github-compact-filetree.user.js
+++ github-compact-filetree.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name        GitHub: Compact file tree
 // @namespace   https://github.com/FelixDombek-TomTom/violentmonkey-userscripts
-// @match       https://github.com/*/*/pull/**
+// @match       https://github.com/**
 // @updateURL   https://github.com/FelixDombek-TomTom/violentmonkey-userscripts/raw/main/github-compact-filetree.user.js
 // @downloadURL   https://github.com/FelixDombek-TomTom/violentmonkey-userscripts/raw/main/github-compact-filetree.user.js
 // @grant       none
-// @version     1.3
+// @version     1.4
 // @author      Felix Dombek
 // @description 7/20/2022, 5:34:29 PM
 // ==/UserScript==
@@ -20,4 +20,4 @@ function addGlobalStyle(css) {
 
 addGlobalStyle('.ActionList-content { padding-top: 1px !important; padding-bottom: 0 !important; font-size: small !important }' +
                'diff-layout .Layout-sidebar { margin-left: -30px !important }' +
-               '.Layout-sidebar, file-tree { width: 320px !important }');
+               'diff-layout .Layout-sidebar, diff-layout file-tree { width: 320px !important }');
