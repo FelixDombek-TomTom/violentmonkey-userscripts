d4dc2e625809d56238244956fb3569341390a43f
Felix Dombek <Felix.Dombek@tomtom.com>
2022-08-11 16:00:06 +0200

    show all ci jobs

--- github-compact-filetree.user.js
+++ github-compact-filetree.user.js
@@ -5,19 +5,23 @@
 // @updateURL   https://github.com/FelixDombek-TomTom/violentmonkey-userscripts/raw/main/github-compact-filetree.user.js
 // @downloadURL   https://github.com/FelixDombek-TomTom/violentmonkey-userscripts/raw/main/github-compact-filetree.user.js
 // @grant       none
-// @version     1.4
+// @version     1.5
 // @author      Felix Dombek
 // @description 7/20/2022, 5:34:29 PM
 // ==/UserScript==
 
 function addGlobalStyle(css) {
-    var head = document.querySelector('head');
-    if (!head) { return; }
-    var style = document.createElement('style');
-    style.innerHTML = css;
-    head.appendChild(style);
+  var head = document.querySelector('head');
+  if (!head) return;
+  var style = document.createElement('style');
+  style.innerHTML = css;
+  head.appendChild(style);
 }
 
-addGlobalStyle('.ActionList-content { padding-top: 1px !important; padding-bottom: 0 !important; font-size: small !important }' +
-               'diff-layout .Layout-sidebar { margin-left: -30px !important }' +
-               'diff-layout .Layout-sidebar, diff-layout file-tree { width: 320px !important }');
+addGlobalStyle(`
+  .ActionList-content { padding-top: 1px !important; padding-bottom: 0 !important; font-size: small !important }
+  diff-layout .Layout-sidebar { margin-left: -30px !important }
+  diff-layout .Layout-sidebar, diff-layout file-tree { width: 320px !important }
+  .merge-status-list { max-height: inherit !important; }
+  .merge-status-item { padding: 1px 8px 1px 1px !important; }
+`);
