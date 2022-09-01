d9f17ae2bef010b11b6cdaa9fa9e418f625a3980
Felix Dombek <Felix.Dombek@tomtom.com>
2022-09-01 15:20:40 +0200

    prevent error on nonmatching pages

--- github-pr-navbar.user.js
+++ github-pr-navbar.user.js
@@ -5,7 +5,7 @@
 // @updateURL   https://github.com/FelixDombek-TomTom/violentmonkey-userscripts/raw/main/github-pr-navbar.user.js
 // @downloadURL   https://github.com/FelixDombek-TomTom/violentmonkey-userscripts/raw/main/github-pr-navbar.user.js
 // @grant       none
-// @version     1.6
+// @version     1.7
 // @author      Felix Dombek
 // @description 8/1/2022, 3:01:57 PM
 // ==/UserScript==
@@ -24,18 +24,26 @@ addGlobalStyle(`
   #own-clonedtabs { margin: -5px 0 0 10px; background-color: rgba(255, 255, 255, 1.0); }
 `);
 
-let tabnavTabs = document.querySelector(".tabnav-tabs");
-let clonedTabs = tabnavTabs.cloneNode(true);
-clonedTabs.querySelectorAll("a").forEach(elem => {
-  elem.classList.add("tooltipped");
-  elem.classList.add("tooltipped-s");
-  elem.setAttribute("aria-label", elem.textContent.trim());
-});
-clonedTabs.setAttribute("id", "own-clonedtabs");
+function createClone() {
+  let tabnavTabs = document.querySelector(".tabnav-tabs");
+  if (!tabnavTabs) { return null; }
+  let clonedTabs = tabnavTabs.cloneNode(true);
+  clonedTabs.querySelectorAll("a").forEach(elem => {
+    elem.classList.add("tooltipped");
+    elem.classList.add("tooltipped-s");
+    elem.setAttribute("aria-label", elem.textContent.trim());
+  });
+  clonedTabs.setAttribute("id", "own-clonedtabs");
+  return clonedTabs;
+}
 
 function appendCloneIfNeeded() {
   if (!document.getElementById("own-clonedtabs")) {
-    document.querySelector('.diffbar.details-collapse h1').appendChild(clonedTabs);
+    let clonedTabs = createClone();
+    if (clonedTabs) {
+      let appendTo = document.querySelector('.js-sticky h1')
+      if (appendTo) appendTo.appendChild(clonedTabs);
+    }
   }
 }
 
